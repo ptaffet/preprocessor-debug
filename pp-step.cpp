6b8d441364bd085b51daa903ede0124df1e73e03
William Killian <william.killian@gmail.com>
2017-07-05 16:51:16 -0700

    Now working

--- pp-step.cpp
+++ pp-step.cpp
@@ -7,6 +7,8 @@
 #include "clang/Basic/SourceManager.h"
 #include "clang/Basic/TargetInfo.h"
 #include "clang/Basic/TargetOptions.h"
+#include "clang/Driver/Driver.h"
+#include "clang/Driver/Options.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendOptions.h"
 #include "clang/Frontend/TextDiagnosticPrinter.h"
@@ -19,42 +21,39 @@
 #include "clang/Lex/PPConditionalDirectiveRecord.h"
 #include "clang/Lex/Preprocessor.h"
 #include "clang/Lex/PreprocessorOptions.h"
-
-#include "clang/Driver/Options.h"
-#include "clang/Driver/Driver.h"
-
-#include "llvm/Option/OptTable.h"
 #include "llvm/Option/ArgList.h"
+#include "llvm/Option/OptTable.h"
 
 using namespace clang;
-
-
 
 struct log_expands : public clang::PPCallbacks {
   log_expands(Preprocessor &p) : pp(p) {}
 
-  virtual void MacroExpands(const Token &MacroNameTok, const MacroDefinition &MD, SourceRange Range,
+  virtual void MacroExpands(const Token &MacroNameTok,
+                            const MacroDefinition &MD, SourceRange Range,
                             const MacroArgs *Args) {
-    llvm::outs() << "expanding macro " << pp.getSpelling(MacroNameTok) << " into " << '\n';
 
-    const auto& macro = *MD.getMacroInfo();
+    llvm::outs() << "expanding macro " << pp.getSpelling(MacroNameTok)
+                 << " into " << '\n';
+    const auto macro = MD.getMacroInfo();
 
-    for (const auto& tok : macro.tokens())
+    for (const auto &tok : macro->tokens())
       llvm::outs() << pp.getSpelling(tok) << ' ';
 
-    if (macro.isFunctionLike() && Args != nullptr) {
+    if (macro->isFunctionLike() && Args != nullptr) {
       llvm::outs() << '\n' << '\t' << "where:" << '\n';
-
-      auto tokenAt = [Args] (unsigned int index) {
-        return Args->getUnexpArgument(index);
+      auto tokenAt = [Args](unsigned int index) {
+        return Args->getUnexpArgument(0) + index;
       };
-
       unsigned int i = 0;
-      for (const auto& args : macro.args()) {
+      for (const auto args : macro->args()) {
         llvm::outs() << '\t' << '\t' << args->getNameStart() << " is ";
-        while (tokenAt(i)->isNot(tok::eof))
-          llvm::outs() << pp.getSpelling(*tokenAt(i++));
+        while (tokenAt(i)->isNot(tok::eof)) {
+          llvm::outs() << pp.getSpelling(*tokenAt(i));
+          i++;
+        }
         i++;
+        llvm::outs() << '\n';
       }
     }
   }
@@ -65,32 +64,57 @@ int main(int argc, char *argv[]) {
 
   CompilerInstance ci;
 
+  // -----========== TARGET ==========-----
+
+  ci.createDiagnostics(
+      new clang::TextDiagnosticPrinter(llvm::errs(), &ci.getDiagnosticOpts()));
+  std::shared_ptr<TargetOptions> topts(new clang::TargetOptions);
+  topts->Triple = LLVM_DEFAULT_TARGET_TRIPLE;
+  ci.setTarget(TargetInfo::CreateTargetInfo(ci.getDiagnostics(), topts));
+
   // -----========== ARGUMENT PARSING ==========-----
 
   using llvm::opt::InputArgList;
-  std::vector <const char*> ref;
+  std::vector<const char *> ref;
   for (int i = 1; i < argc; ++i)
     ref.emplace_back(argv[i]);
   unsigned missingIndex{0}, missingCount{0};
   auto table = clang::driver::createDriverOptTable();
   auto Args = table->ParseArgs(ref, missingIndex, missingCount);
 
+  // -----========== FILE ==========-----
+
+  ci.createFileManager();
+  ci.createSourceManager(ci.getFileManager());
+
+  auto inputFile = [&]() -> FileID {
+    auto inputs = Args.getAllArgValues(clang::driver::options::OPT_INPUT);
+    if (inputs.size() == 1 && inputs[0] != "-") {
+      const FileEntry *pFile = ci.getFileManager().getFile(inputs[0]);
+      return ci.getSourceManager().getOrCreateFileID(
+          pFile, clang::SrcMgr::CharacteristicKind::C_User);
+    }
+    llvm::errs() << "None or More than one source file was specified -- aborting\n";
+    exit(1);
+    return ci.getSourceManager().createFileID(nullptr);
+  }();
+
   // -----========== PREPROCESSOR ==========-----
   {
     // include directory defaults
-    const char* dirs = {
+    const char *dirs = {
       #ifdef SYSTEM_HEADERS
-        SYSTEM_HEADERS ,
+        SYSTEM_HEADERS,
       #endif
-      nullptr
+        nullptr
     };
-    for (const char* dir = dirs; dir != nullptr; ++dir)
+    for (const char *dir = dirs; dir != nullptr; ++dir)
       ci.getHeaderSearchOpts().AddSystemHeaderPrefix(dir, true);
 
     using namespace clang::driver::options;
     using namespace llvm::opt;
 
-    for (const Arg *A: Args.filtered(OPT_I))
+    for (const Arg *A : Args.filtered(OPT_I))
       ci.getHeaderSearchOpts().AddPath(A->getValue(), frontend::Quoted, false, true);
     for (const Arg *A : Args.filtered(OPT_D, OPT_U)) {
       if (A->getOption().matches(OPT_D))
@@ -104,37 +128,17 @@ int main(int argc, char *argv[]) {
     for (const Arg *A : Args.filtered(OPT_chain_include))
       ci.getPreprocessorOpts().ChainedIncludes.emplace_back(A->getValue());
   }
-  ci.createPreprocessor(clang::TranslationUnitKind::TU_Complete); // create Preprocessor
-
-  // -----========== TARGET ==========-----
-
-  ci.createDiagnostics(new clang::TextDiagnosticPrinter(llvm::errs(), &ci.getDiagnosticOpts()));
-  std::shared_ptr<TargetOptions> topts(new clang::TargetOptions);
-  topts->Triple = LLVM_DEFAULT_TARGET_TRIPLE;
-  ci.setTarget(TargetInfo::CreateTargetInfo(ci.getDiagnostics(), topts));
-
-  // -----========== FILE ==========-----
-
-  ci.createFileManager();
-  ci.createSourceManager(ci.getFileManager());
-
-  auto inputFile = [&] () -> FileID {
-    auto inputs = Args.getAllArgValues(clang::driver::options::OPT_INPUT);
-    if (inputs.size() == 1 && inputs[0] != "-") {
-      const FileEntry *pFile = ci.getFileManager().getFile(inputs[0]);
-      return ci.getSourceManager().getOrCreateFileID(pFile, clang::SrcMgr::CharacteristicKind::C_User);
-    }
-    llvm::errs() << "None or More than one source file was specified -- aborting\n";
-    exit(1);
-    return ci.getSourceManager().createFileID(nullptr);
-  }();
+  ci.createPreprocessor(
+      clang::TranslationUnitKind::TU_Complete); // create Preprocessor
 
   // -----========== APPLY PASS ==========-----
 
   ci.getSourceManager().setMainFileID(inputFile);
   ci.getPreprocessor().EnterMainSourceFile();
-  ci.getDiagnosticClient().BeginSourceFile(ci.getLangOpts(), &ci.getPreprocessor());
-  ci.getPreprocessor().addPPCallbacks(std::unique_ptr<log_expands>(new log_expands(ci.getPreprocessor())));
+  ci.getDiagnosticClient().BeginSourceFile(ci.getLangOpts(),
+                                           &ci.getPreprocessor());
+  ci.getPreprocessor().addPPCallbacks(
+      std::unique_ptr<log_expands>(new log_expands(ci.getPreprocessor())));
 
   Token tok;
   do {
